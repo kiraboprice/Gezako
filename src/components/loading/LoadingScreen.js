00e655898f823ef6085622ebe15d2eadc0aa8928
power <powermukisa@gmail.com>
2020-01-06 19:02:48 +0300

    Add loading screen to home screen

---
+++ src/components/loading/LoadingScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import loading_gif  from "../../assets/Imgs/loading.gif";
+
+import "./loading.css"
+
+const LoadingScreen = () =>{
+  return(
+      <div className="loading">
+        <div className="load-gif">
+          <img src={loading_gif} alt="loading reports" />
+        </div>
+      </div>
+  )
+}
+
+export default LoadingScreen;
