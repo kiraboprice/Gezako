0c1724cc863add92747d7caa749a4e1d1eeece7f
power <powermukisa@gmail.com>
2020-01-21 13:34:09 +0300

    add update stats function

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+import {FieldValue} from "firebase";
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
@@ -5,42 +6,59 @@ admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 const BASE_DOCUMENT = 'company/tala';
 
-exports.incrementNumberOfTestsForService = functions.firestore
+exports.incrementNumberOfTestsForServiceOnCreate = functions.firestore
 .document(`${BASE_DOCUMENT}/completedreports/{id}`)
 .onCreate((snap, context) => {
-  return incrementNumberOfTestsForService(context.params.id, snap.data());
+  return incrementNumberOfTestsForService(context.params.id, snap.data(), null);
 });
 
-function incrementNumberOfTestsForService(id, report) {
-  return db.collection(`${BASE_DOCUMENT}/reportstats`).doc(report.service).get()
+exports.incrementNumberOfTestsForServiceOnUpdate = functions.firestore
+.document(`${BASE_DOCUMENT}/completedreports/{id}`)
+.onUpdate((change, context) => {
+  return incrementNumberOfTestsForService(context.params.id, change.after.data(), change.before.data());
+});
+
+
+
+function incrementNumberOfTestsForService(id, newReport, oldReport) {
+  return db.collection(`${BASE_DOCUMENT}/reportstats`).doc(newReport.service).get()
   .then(doc => {
     if (!doc.exists) {
       //create report stats for that service if it doesnt exist
-      console.log(`Service entry doesnt exist. Creating entry for ${report.service}`);
-      db.collection(`${BASE_DOCUMENT}/reportstats`).doc(report.service).set(
-          {numberOfTests: report.numberOfTests}
+      console.log(`Service entry doesnt exist. Creating entry for ${newReport.service}`);
+      db.collection(`${BASE_DOCUMENT}/reportstats`).doc(newReport.service).set(
+          {
+            numberOfTests: newReport.numberOfTests,
+            updatedAt: FieldValue.serverTimestamp()
+          }
       ).then(
-          console.log(`Created entry for ${report.service}`)
+          console.log(`Created entry for ${newReport.service}`)
       )
     } else {
-      //get current report stats for service
-      console.log('Service entry exists: getting report starts for doc')
+      //update report stats for service
+      console.log('Service entry exists: updating report stats..');
+      updateReportStats(doc.data(), oldReport, newReport)
     }
   })
   .catch(err => {
     console.log('incrementNumberOfTestsForService FAILED: ', err)
   });
+}
 
-  // //get current report stats for service
-  // db.doc(`company/tala/reportstats/${report.service}`)
-  // .get()
-  //
-  // //update report stats
-  // const numberOfTests = report.prevNumberOfTests;
-  // const service = report.service;
-  // const time = db.FieldValue.serverTimestamp();
-  //
-  // return db.doc(`company/tala/reportstats/${service}`)
-  // .update()
-  // .then(doc => console.log('updateNumberOfTestsForService', doc))
+function updateReportStats(reportStats, oldReport, newReport) {
+  const oldNumberOfTests = parseInt(oldReport.numberOfTests);
+  const newNumberOfTests = parseInt(newReport.numberOfTests);
+  const numberOfTestsToIncrementBy = newNumberOfTests - oldNumberOfTests;
+
+  //todo this assumes that the service has not been updated. Remove ability to update the service coz why would you do this anyway?
+  return db.collection(`${BASE_DOCUMENT}/reportstats`).doc(newReport.service)
+  .update({
+    numberOfTests: reportStats.numberOfTests + numberOfTestsToIncrementBy,
+    updatedAt: FieldValue.serverTimestamp()
+  })
+  .then(doc => {
+    console.log('successfully updated Number Of Tests For Service', doc)})
+  .catch(err => {
+    console.log('failed to update number Of Tests For service FAILED: ', err)
+  });
 }
