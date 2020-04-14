3cf9a13b99db0fea7f52381c10bc4f60579eddb9
Codebreake <38604311+Codebreake@users.noreply.github.com>
2020-04-14 18:32:59 +0300

    Sample drop down (#79)
    Add logout menu to user profile
    
    
    Co-authored-by: power <powermukisa@gmail.com>

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,26 +3,53 @@ import React, { useState } from 'react';
 import './navbar.css';
 
 import {connect} from 'react-redux';
-import {signOut} from '../../store/actions/authActions'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import {signOut} from "../../store/actions/authActions";
 
 const NavBar = (props) => {
-  const { user, signOut } = props;
-  const [displaySignOutButton, setDisplaySignOutButton] = useState('none');
+  const { user } = props;
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const { signOut } = props;
+
+  const handleOnClickProfilePhoto = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOnClickLogout = () =>{
+    signOut()
+  };
 
   return (
       <div id='navigation-top'>
         <div id='navigation-logo'>
           Gezako
         </div>
-        <div id='signout' style={{display: displaySignOutButton}} onClick={signOut}>
-          Sign out
-        </div>
         <div id='profile-picture'>
 
-        <img src={user.photoURL} alt={user.displayName} onClick={
-          () => displaySignOutButton === 'none' ? setDisplaySignOutButton('block') : setDisplaySignOutButton('none')
-        }></img>
+          <img src={user.photoURL} alt={user.displayName}
+               aria-controls="simple-menu" aria-haspopup="true"
+               position="relative"
+               onClick={handleOnClickProfilePhoto}
+          ></img>
 
+          <Menu
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              style={{marginTop: "55px"}}
+          >
+
+            <MenuItem>{user.displayName}</MenuItem>
+            <MenuItem onClick={() => handleOnClickLogout()}>Log out</MenuItem>
+
+          </Menu>
         </div>
       </div>
   )
